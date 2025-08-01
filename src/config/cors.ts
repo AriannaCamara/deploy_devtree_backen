11cf3018c92f5fcdb731d61eafc4319554de2614
Arianna Camara <ariannaisabel795@gmail.com>
2025-08-01 09:36:15 +0200

    fix: agregar console.log para depurar error de CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,17 @@
 import { CorsOptions } from 'cors'
 
-export const corsConfig : CorsOptions = {
-        origin: function(origin, callback) {
+export const corsConfig: CorsOptions = {
+    origin: function (origin, callback) {
+        const whiteList = [
+            process.env.FRONTED_URL,
+            'https://devtree-types.netlify.app',
+            undefined
+        ]
 
-            const whiteList = [process.env.FRONTED_URL]
+        console.log('🔍 CORS Origin:', origin)
+        console.log('📦 FRONTED_URL desde .env:', process.env.FRONTED_URL)
 
-            if(process.argv[2] === '--api') {
-                whiteList.push(undefined)
-            }
-
-            if(whiteList.includes(origin)) {
+        if (whiteList.includes(origin)) {
             callback(null, true)
         } else {
             callback(new Error('Error de CORS'))
